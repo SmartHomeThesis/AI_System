7e9e02f8d489450508c6031e7b3c660bbde9a6a7
angushhz <hoangnamqt.2001@gmail.com>
2023-01-02 20:24:51 +0700

    add: train face

--- authenticate.py
+++ authenticate.py
@@ -58,24 +58,24 @@ class FaceRecognition:
             if not ret:
                 print("failed to grab frame")
                 break
-            small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
+            # small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
+            #
+            # # Convert the image from BGR color (which OpenCV uses) to RGB color (which face_recognition uses)
+            # rgb_small_frame = small_frame[:, :, ::-1]
 
-            # Convert the image from BGR color (which OpenCV uses) to RGB color (which face_recognition uses)
-            rgb_small_frame = small_frame[:, :, ::-1]
-
-            # Find all the faces and face encodings in the current frame of video
-            self.face_locations = face_recognition.face_locations(rgb_small_frame)
-            for (top, right, bottom, left) in zip(self.face_locations):
-                print(top, right, bottom, left)
-                # Scale back up face locations since the frame we detected in was scaled to 1/4 size
-                top *= 4
-                right *= 4
-                bottom *= 4
-                left *= 4
-
-                # Create the frame with the name
-                cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
-                cv2.rectangle(frame, (left, bottom - 35), (right, bottom), (0, 0, 255), cv2.FILLED)
+            # # Find all the faces and face encodings in the current frame of video
+            # self.face_locations = face_recognition.face_locations(rgb_small_frame)
+            # for (top, right, bottom, left) in zip(self.face_locations):
+            #     print(top, right, bottom, left)
+            #     # Scale back up face locations since the frame we detected in was scaled to 1/4 size
+            #     top *= 4
+            #     right *= 4
+            #     bottom *= 4
+            #     left *= 4
+            #
+            #     # Create the frame with the name
+            #     cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
+            #     cv2.rectangle(frame, (left, bottom - 35), (right, bottom), (0, 0, 255), cv2.FILLED)
 
                 # Display the resulting image
             cv2.imshow("press space to take a photo", frame)
@@ -191,10 +191,10 @@ class FaceRecognition:
             cv2.imshow('Face Recognition', frame)
 
             # Authenticate successfully
-            # if self.cnt == 5:
-            #     video_capture.release()
-            #     cv2.destroyAllWindows()
-            #     return True, name[:name.index(".")].upper()
+            if self.cnt == 5:
+                video_capture.release()
+                cv2.destroyAllWindows()
+                return True, name[:name.index(".")].upper()
 
             # Hit 'q' on the keyboard to quit!
             if cv2.waitKey(1) == ord('q'):
