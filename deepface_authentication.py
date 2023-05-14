887ffc7cfb07ced7a920531e24d422b67a9d7e56
angushhz <hoangnamqt.2001@gmail.com>
2023-05-14 15:13:29 +0700

    add FPS display

--- deepface_authentication.py
+++ deepface_authentication.py
@@ -23,9 +23,19 @@ metrics = ["cosine", "euclidean", "euclidean_l2"]
 
 
 cap = cv2.VideoCapture(0)
+prev_frame_time = 0
+# used to record the time at which we processed current frame
+new_frame_time = 0
+
 while(True):
     ret, frame = cap.read()
-    detected_face = DeepFace.extract_faces(frame, detector_backend='opencv',enforce_detection=False)
+    new_frame_time = time.time()
+    fps = 1 / (new_frame_time - prev_frame_time)
+    prev_frame_time = new_frame_time
+            # Only process every other frame of video to save time
+            # Display the FPS on the frame
+    cv2.putText(frame, "FPS: {:.2f}".format(fps), (10, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2)
+    detected_face = DeepFace.extract_faces(frame, detector_backend='mtcnn',enforce_detection=False)
     name_detected= "Unknown"
     facial_area = detected_face[0]['facial_area']
     if detected_face[0]['confidence'] > 0:
@@ -37,8 +47,8 @@ while(True):
       name_detected = parts[-1] # Last part contains the name
     print("NAME_Dectect",name_detected)
     cv2.rectangle(frame, (facial_area['x'],facial_area['y']), (facial_area['x'] + facial_area['w'], facial_area['y']+facial_area['w']), (0, 255, 0), 2)
+    cv2.putText(frame, name_detected, (300, 400), cv2.FONT_HERSHEY_SIMPLEX, 1, (255,0,0), 2)
     cv2.imshow("Face", frame)
-    cv2.putText(frame,name_detected,(facial_area['x']+6,facial_area['y']+6), cv2.FONT_HERSHEY_SIMPLEX, 0.8,(255,255,255),2,cv2.LINE_AA)
     if cv2.waitKey(1) & 0xFF == ord('q'):
           break
 cv2.destroyAllWindows()
