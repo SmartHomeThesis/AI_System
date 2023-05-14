01f9470630ae6337fcfe8d4326922f57480641e0
angushhz <hoangnamqt.2001@gmail.com>
2023-05-14 15:31:31 +0700

    fix error when can't catch face

--- deepface_authentication.py
+++ deepface_authentication.py
@@ -22,7 +22,9 @@ models = [
 metrics = ["cosine", "euclidean", "euclidean_l2"]
 
 
-cap = cv2.VideoCapture(0)
+# cap = cv2.VideoCapture(0)
+# Using in NUC
+cap = cv2.VideoCapture(0,cv2.CAP_DSHOW) 
 prev_frame_time = 0
 # used to record the time at which we processed current frame
 new_frame_time = 0
@@ -35,16 +37,18 @@ while(True):
             # Only process every other frame of video to save time
             # Display the FPS on the frame
     cv2.putText(frame, "FPS: {:.2f}".format(fps), (10, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2)
-    detected_face = DeepFace.extract_faces(frame, detector_backend='mtcnn',enforce_detection=False)
+    detected_face = DeepFace.extract_faces(frame, detector_backend='opencv',enforce_detection=False)
     name_detected= "Unknown"
     facial_area = detected_face[0]['facial_area']
     if detected_face[0]['confidence'] > 0:
       # draw rectangle over face and open cv window base on return fron extract_faces {'x': 211, 'y': 80, 'w': 118, 'h': 151}
-      response = DeepFace.find(img_path = frame, db_path = "faces/train_database", model_name = models[1], distance_metric = metrics[0], enforce_detection = False, detector_backend = 'opencv')
-      path_name_image= response[0]['identity'][0]
-      dirpath, filename = os.path.split(path_name_image)
-      parts = dirpath.split("\\") # Use backslash as separator
-      name_detected = parts[-1] # Last part contains the name
+      response = DeepFace.find(img_path = frame, db_path = "faces/train_database", model_name = models[2], distance_metric = metrics[0], enforce_detection = False, detector_backend = 'opencv')
+      print(len(response[0]))
+      if  len(response[0]) != 0:
+        path_name_image= response[0]['identity'][0]
+        dirpath, filename = os.path.split(path_name_image)
+        parts = dirpath.split("\\") # Use backslash as separator
+        name_detected = parts[-1] # Last part contains the name
     print("NAME_Dectect",name_detected)
     cv2.rectangle(frame, (facial_area['x'],facial_area['y']), (facial_area['x'] + facial_area['w'], facial_area['y']+facial_area['w']), (0, 255, 0), 2)
     cv2.putText(frame, name_detected, (300, 400), cv2.FONT_HERSHEY_SIMPLEX, 1, (255,0,0), 2)
